510833ecf9888bb632d34b16a6fb42970d6e5b96
jeremy <jw.gim@outlook.com>
2025-01-28 17:46:55 +0900

    chore: 초기 세팅

---
+++ mobile/babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
+};
